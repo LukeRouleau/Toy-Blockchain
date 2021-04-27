fcd5f273fe68ad531773a2e9f4914cbf3b908fab
LukeRouleau <lucasluke2000@gmail.com>
2021-04-27 11:17:46 -0400

    v0.0

--- blockchain_python/blockchain.py
+++ blockchain_python/blockchain.py
@@ -7,5 +7,29 @@ Dependencies: Flask & requests, you'll also need an HTTP client like Postman
 Purpose: To create basic blockchain in python
 '''
 
+# Create a blockchain class to store the chain:
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
 
+    def new_block(self):
+        # Create a new block and add it to the chain
+        pass
+
+    def new_transaction(self):
+        # Add a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        # Return the last block in the chain
+        pass
+
+    
 
