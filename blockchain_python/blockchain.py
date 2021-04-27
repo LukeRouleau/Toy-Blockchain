714a7d7c53bffd4b0e5e076b3beffc197dd9f468
LukeRouleau <lucasluke2000@gmail.com>
2021-04-27 11:02:10 -0400

    Initial Commit

---
+++ blockchain_python/blockchain.py
@@ -0,0 +1,11 @@
+'''
+Filename: blockchain.py
+Author: Luke Rouleau
+Date Created: 4/27/2021
+Last Edit: 4/27/2021
+Dependencies: Flask & requests, you'll also need an HTTP client like Postman
+Purpose: To create basic blockchain in python
+'''
+
+
+
