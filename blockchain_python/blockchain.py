946c8d56d804ed81c17963676e20ada3e276449c
LukeRouleau <lucasluke2000@gmail.com>
2021-04-27 14:54:57 -0400

    v0.3

--- blockchain_python/blockchain.py
+++ blockchain_python/blockchain.py
@@ -1,11 +1,16 @@
+#%%
 '''
 Filename: blockchain.py
 Author: Luke Rouleau
 Date Created: 4/27/2021
 Last Edit: 4/27/2021
 Dependencies: Flask & requests, you'll also need an HTTP client like Postman
+Sources:
+        https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
+        https://flask.palletsprojects.com/en/1.1.x/tutorial/layout/
 Purpose: To create basic blockchain in python
 '''
+
 
 import hashlib
 import json
@@ -34,11 +39,11 @@ class Blockchain(object):
         :return: <dict> New Block
         """
         block = {
-            'index' : len(self.chain) + 1,
-            'timestamp' : time(),
-            'transactions' : self.current_transactions,
-            'proof' : proof,
-            'previous_hash' : previous_hash or self.hash(self.chain[-1]),
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
         
         # Reset the current list of transactions as we have already inserted it in the new block above
@@ -56,9 +61,9 @@ class Blockchain(object):
         """
 
         self.current_transactions.append({
-            'sender' : sender,
-            'recipient' : recipient,
-            'amount' : amount,
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
         })
         # return the index of the block which the transaction will be added to, next one mined
         return self.last_block['index'] + 1
@@ -84,9 +89,10 @@ class Blockchain(object):
         :param proof: <int> Current Proof
         :return: <bool> True if correct, False if not.
         """
-        guess = f'{last_proof}{proof}'.encode()
+        # I'm using a python f string here to compute the product inside of the string
+        guess = f'{last_proof}*{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
+        return guess_hash[:4] == "0000" # increasing this size makes a massive in time
 
 
     # I'm still not entirely clear on the nature of staticmethods, 
@@ -108,5 +114,77 @@ class Blockchain(object):
         # Return the last block in the chain
         pass
 
+#%%
+
+# Instantiate a server as a node in our blockchain
+app = Flask(__name__)
+
+# Generate a globally Universially Unique ID address for this node
+node_identifier = str(uuid4()).replace('-','')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # Run the POW algorithm
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+    
+    # Receive a reward for finding the proof
+    # Sender = '0' tpo signify that this node has mined a new coin
+    blockchain.new_transaction(
+        sender = "0",
+        recipient = node_identifier,
+        amount = 1,
+    )
+
+    # Forge new block by adding it to the chain
+    previous_hash = blockchain.hash(last_block) # grab the hash of the last block
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+#%%
+# Sandbox:
+node_identifier = str(uuid4()).replace('-','')
+print(node_identifier)
+print(str(uuid4()))
+
+# %%
